f6c0a69e38c0262ae7ea86f6662e8640460c0031
josephdattilo03 <jadattilobusiness@gmail.com>
2023-11-24 23:13:56 -0500

    introduced link icons

---
+++ portfolio-website/src/components/home/IPodUI.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {motion} from 'framer-motion'
+
+interface IPodUIProps {
+    menu: string
+}
+
+export default function IPodUI({menu} : IPodUIProps) {
+    const text = 'Hi there! Welcome to my portfolio. To have a look around, press the center button below.'
+    return (
+        <div>{text.split('').map((letter, index) => {
+            return (
+                <motion.span style={{color: 'white'}} 
+                initial={{opacity: 0}} 
+                animate={{opacity: 1}} 
+                transition={{delay: .05*(index + 1), duration: 0}}>{letter}</motion.span>
+            )
+        })}</div>
+    )
+}
